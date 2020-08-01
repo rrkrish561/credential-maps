667d54041da806061b5f6c01756c3817a9f28a5c
matt-iknow <60084825+matt-iknow@users.noreply.github.com>
2020-08-01 15:17:33 -0400

    Added getters+setters, isLowerKeyValue function, insertNode traversal

--- bst.cpp
+++ bst.cpp
@@ -1,11 +1,47 @@
 #include "bst.h"
 
+BST::~BST(){
+    //some traversal that progressively deletes leaves
+}
+
+
 void BST::insertNode(string key, string value){
     TreeNode* newnode = new TreeNode(key,value);
     if(_size == 0){
         _root = newnode;
+        _size++;
     }else{
-        //TODO: determine where the node should be placed in the tree
+        TreeNode* currnode = _root;
+        TreeNode* prevnode;
+        bool lower = false;
+
+        while(currnode != nullptr){
+            string currnode_key = currnode->getKey();
+            if(key.compare(currnode->getKey()) == 0){ //if the keys are equal
+                prevnode = nullptr;//duplicate found, do not insert anything
+                currnode = nullptr;
+                delete newnode;
+            }else{
+                lower = isLowerKeyValue(key, currnode_key);
+                if(lower){
+                    prevnode = currnode;
+                    currnode = currnode->getLeft();
+                }else{
+                    prevnode = currnode;
+                    currnode = currnode->getRight();
+                }
+            }
+        }
+
+        if(prevnode != nullptr){
+            if(lower)
+                prevnode->setLeft(newnode);
+            else
+                prevnode->setRight(newnode);
+            _size++;
+        }
+
+
     }
 }
 
@@ -15,10 +51,11 @@ BST::TreeNode* BST::getNode(string key){
     }else{
         BST::TreeNode* currnode = _root;
         while(currnode != nullptr){
-            if(currnode->isMatchingKey(key)){
+            string currnode_key = currnode->getKey();
+
+            if(key.compare(currnode_key) == 0){ //if the keys are equal
                 return currnode;
             }else{
-                string currnode_key = currnode->getKey();
                 if(isLowerKeyValue(key, currnode_key))
                     currnode = currnode->getLeft();
                 else
@@ -29,14 +66,50 @@ BST::TreeNode* BST::getNode(string key){
     }
 }
 
-bool BST::isCorrectCredentials(string key, string value){
-    BST::TreeNode* currnode = getNode(key);
-    if(currnode == nullptr){
-        return false;
-    }else{
-        if(currnode->isMatchingValue(value))
-            return true;
-        else
-            return false;
+bool BST::isLowerKeyValue(string k1, string k2){
+    unsigned int num_iters;
+    bool isLowerChar = false;
+    
+    //number of comparisons depends on length of shorter string
+    if(k1.length() > k2.length())
+        num_iters = k2.length();
+    else
+        num_iters = k1.length();
+
+    for(unsigned int i = 0 ; i < num_iters; i++){
+        isLowerChar = k1[i] < k2[i]; 
+        if(isLowerChar) //if there is one instance where this is true, k1 has a lower overall value.
+            break;
     }
+    return isLowerChar;
+}
+
+BST::TreeNode::TreeNode(string key, string value){
+    _key = key;
+    _value = value;
+}
+void BST::TreeNode::setLeft(TreeNode* left){
+    _left = left;
+}
+void BST::TreeNode::setRight(TreeNode* right){
+    _right = right;
+}
+void BST::TreeNode::setIsLeaf(bool isLeaf){
+    _isLeaf = isLeaf;
+}
+
+string BST::TreeNode::getKey(){
+    return _key;
+}
+string BST::TreeNode::getValue(){
+    return _value;
+}
+bool BST::TreeNode::getIsLeaf(){
+    return _isLeaf;
+}
+BST::TreeNode* BST::TreeNode::getLeft(){
+    return _left;
+}
+BST::TreeNode* BST::TreeNode::getRight(){
+    return _right;
 }
--- bst.h
+++ bst.h
@@ -5,26 +5,27 @@ struct BST{
     private:
         struct TreeNode{
             private:
-                TreeNode* _left;
+                TreeNode* _left; //init nullptr
                 TreeNode* _right;
                 string _key;
                 string _value;
-                bool isLeaf;
+                bool _isLeaf;
             public:
                 TreeNode(string key, string value);
                 string getKey();
-                //void setKey(string key);
-                //void setValue(string value);
+                string getValue();
+                bool getIsLeaf();
+                void setIsLeaf(bool isLeaf);
                 TreeNode* getLeft();
                 TreeNode* getRight();
-                bool isMatchingKey(string key);
-                bool isMatchingValue(string value);
+                void setLeft(TreeNode* left);
+                void setRight(TreeNode* right);
         };
         TreeNode* _root;
         unsigned int _size;
         bool isLowerKeyValue(string k1, string k2); //returns true if k1 has a lower value than k2, based on char values
     public:
+        ~BST();
         TreeNode* getNode(string key);
         void insertNode(string key, string value);
-        bool isCorrectCredentials(string key, string value);
 };

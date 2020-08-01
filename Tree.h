46b8e90aef1320f0e638291849968fc1606fde3b
Raghu Radhakrishnan <rrkrish561@gmail.com>
2020-08-01 16:54:47 -0400

    tree interface

---
+++ Tree.h
@@ -0,0 +1,17 @@
+class Tree
+{
+  protected:
+    struct TreeNode
+    {
+      string key;
+      string value;
+      TreeNode* right;
+      TreeNode* left;
+    };
+
+  public:
+    virtual void Insert(string key, string value) = 0;
+    virtual void Delete(string key) = 0;
+    virtual string Search(string key) = 0;
+};
+

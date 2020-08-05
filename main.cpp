48f0823dcc241ad40324ff5a2a9481faea5aaf77
matt-iknow <60084825+matt-iknow@users.noreply.github.com>
2020-08-05 16:20:08 -0400

    liam's main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "BST.h"
-#include "SplayTree.h"
-#include "CredentialsInserter.h"
 #include <vector>
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include "CredentialsInserter.h"
 #include <chrono>
+#include "Tree.h"
+#include "SplayTree.h"
 #include "hashlibpp.h"
 
 using namespace std::chrono;
@@ -15,20 +16,19 @@ int main()
     typedef high_resolution_clock Clock;
     int menuSelect = 0;
     int treeSpecifier;
-    string userName;
-    string passWord;
     hashwrapper *passWrapper = new md5wrapper();
-    string hashedPass;
     BST * bstPointer = new BST();
     SplayTree * splayPointer = new SplayTree();
     CredentialsInserter bstInserter = CredentialsInserter(bstPointer);
     CredentialsInserter splayInserter = CredentialsInserter(splayPointer);
 
-    string insertCredTextFile = "";
-    string loginSimulatorFile = "";
+    string insertCredTextFile = "usernameData_1000.txt";
+    string loginSimulatorFile = "variedlogins_1000_1to3.txt";
 
     while(menuSelect!=4)
     {
+        string userName;
+        string passWord;
         cout << "-----Main Menu-----\n 1. Login\n 2. Register\n 3. Test Logins" << endl;
         cin >> menuSelect;
         switch(menuSelect)
@@ -40,7 +40,6 @@ int main()
                 cout << "\n";
                 cout << "Please enter your password: ";
                 cin >> passWord;
-                hashedPass = passWrapper->getHashFromString(passWord);
                 cout << "Please specify which tree to search in:\n 1. BST\n 2. Splay Tree" << endl;
                 cin >> treeSpecifier;
                 if(treeSpecifier==1)
@@ -56,17 +55,20 @@ int main()
                         cout << "Account not found!" << endl;
                     }
                 }
-                else if(treeSpecifier==2)
+                else
                 {
-                    auto t1 = Clock::now();
-                    if (splayInserter.verifyCreds(userName, passWord))
+                    if(treeSpecifier==2)
                     {
-                        auto t2 = Clock::now();
-                        cout << "Account found in Splay Tree in " << duration_cast<nanoseconds>(t2 - t1).count() << " nanoseconds." << endl;
-                    }
-                    else
-                    {
-                        cout << "Account not found!" << endl;
+                        auto t1 = Clock::now();
+                        if (splayInserter.verifyCreds(userName, passWord))
+                        {
+                            auto t2 = Clock::now();
+                            cout << "Account found in Splay Tree in " << duration_cast<nanoseconds>(t2 - t1).count() << " nanoseconds." << endl;
+                        }
+                        else
+                        {
+                            cout << "Invalid credentials!" << endl;
+                        }
                     }
                 }
                 break;
@@ -78,7 +80,7 @@ int main()
                 cout << "\n";
                 cout << "Please enter a password: ";
                 cin >> passWord;
-                
+
                 cout << "Inserting account into BST and Splay Tree..." << endl;
 
                 bstInserter.insertCreds(userName, passWord);
@@ -89,29 +91,55 @@ int main()
             {
                 // Insert and time credentials into both trees
 
-
                 ifstream fileReader;
                 string userN;
                 string passW;
-                vector<pair<string,string>> accounts;
+                string dataHolder;
+                fileReader.open(insertCredTextFile, ios::binary);
+                auto t1 = Clock::now();
+                while(getline(fileReader, dataHolder))
+                {
+                    stringstream ss(dataHolder);
+                    ss >> userN >> passW;
+                    bstInserter.insertCreds(userN, passW);
+                }
+                auto t2 = Clock::now();
+                fileReader.close();
+                int time = duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+                cout << "Time to generate BST: " << time << "nanoseconds." <<endl;
+
+                fileReader.open(insertCredTextFile, ios::binary);
+                t1 = Clock::now();
+                while(getline(fileReader, dataHolder))
+                {
+                    stringstream ss(dataHolder);
+                    ss >> userN >> passW;
+                    splayInserter.insertCreds(userN, passW);
+                }
+                t2 = Clock::now();
+                fileReader.close();
+                time = duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+                cout << "Time to generate Splay Tree: " << time << "nanoseconds." << endl;
 
                 fileReader.open(loginSimulatorFile, ios::binary);
-                string dataHolder;
+                vector<pair<string,string>> accounts;
                 while(getline(fileReader, dataHolder))
                 {
                     stringstream ss(dataHolder);
                     ss >> userN >> passW;
                     pair<string, string> account = make_pair(userN, passW);
                     accounts.push_back(account);
- 
+
                 }
-                auto t1 = Clock::now();
+                fileReader.close();
+
+                t1 = Clock::now();
                 for(unsigned int i = 0; i < accounts.size(); i++)
                 {
                     bstInserter.verifyCreds(accounts[i].first, accounts[i].second);
                 }
-                auto t2 = Clock::now();
-                int time = duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
+                t2 = Clock::now();
+                time = duration_cast<std::chrono::nanoseconds>(t2 - t1).count();
                 double averageTime = time / (double) accounts.size();
                 cout << "Average login time in BST is " << averageTime << " nanoseconds." << endl;
 
